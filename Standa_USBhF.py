36d4ab3bf5ec9bcd1a7a22ac0bc6740676c93491
Sam Hutchings <samuel.hutchings@btinternet.com>
2016-07-11 16:27:02 +0100

    Created Module

---
+++ Standa_USBhF.py
@@ -0,0 +1,47 @@
+import subprocess
+
+
+class ListDevices(object):
+
+    def getDeviceID(id_code):
+        fileLocation = "./binaries/ListConnectedStandaDevices.exe"
+        ID = subprocess.Popen(fileLocation, creationflags=0x08000000)
+        ID.wait()
+
+
+class ControllerInterface(object):
+    device_id = 0
+
+    def __init__(self, device_id):
+        self.device_id = device_id
+
+    def talk(self, parameterString):
+        fileLocation = "./binaries/StandaStepperMotorController.exe " + \
+            str(self.device_id) + " " + parameterString
+        result = subprocess.Popen(fileLocation, creationflags=0x08000000)
+        result.wait()
+        return result
+
+    def powerOn(self):
+        return self.talk("on")
+
+    def powerOff(self):
+        return self.talk("off")
+
+    def step(self, steps):
+        return self.talk("step " + str(steps))
+
+    def goToPosition(self, position):
+        return self.talk("goto " + str(position))
+
+    def stop(self):
+        return self.talk("stop")
+
+    def setHome(self):
+        return self.talk("setHome")
+
+    def getPosition(self):
+        return self.talk()
+
+    def close(self):
+        return self.talk("close")
